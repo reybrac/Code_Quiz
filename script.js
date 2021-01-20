343f26f0976417cca28b7810f3526b25764ed796
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-01-20 09:49:29 -0800

    updates to the object structure

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-var hello = document.querySelector("#myBtn");
+var start = document.querySelector("#myBtn");
 var secondsLeft = 10;
 document.getElementById("myText").innerHTML = secondsLeft; 
 var timeEl = document.querySelector(".time");
 
-var questions = {question1: "A very useful tool used during development and debugging for printing content to the debugger is:"};
-var choices = {choices1: ["Javascript", "Terminal bash", "for Loops", "Console log"]};
-var accuracy = {accuracy1: "Console log"};
+// var questions = {question1: "A very useful tool used during development and debugging for printing content to the debugger is:"
+//                 ,question2: "What is the best food?"};
+// var choices = {choices1: ["Javascript", "Terminal bash", "for Loops", "Console log"]
+//                 ,choices2:["pizza", "burgers", "anchovies"]};
+// var accuracy = {accuracy1: "Console log", accuracy2: "burgers"};
 
-var quizChoices = document.querySelector("#accuracy")
+var exampleQues = [
+    {
+        question: "What is the best food?",
+        choices: ["pizza", "burgers", "anchovies"],
+        accuracy: ["burgers"]
+    },
+    {
+        question: "What is the best drink?",
+        choices: ["Ginger Ale", "Coca Cola", "Fanta", "Dr. Pepper"],
+        accuracy: "Ginger Ale"
+    }
+];
 
-hello.addEventListener("click", function(){
+var quizChoices = document.querySelector("#choices")
+
+start.addEventListener("click", function(){
     init();
     setTime();  
     quiz();
     
 });
+
 
 function setTime() {
     document.getElementById("myBtn").disabled = true;
@@ -37,26 +53,44 @@ function setTime() {
     
 }
 
+
+
+
 function quiz() {
     
-    document.getElementById("questions").innerHTML = questions.question1;
-    console.log(questions.question1);
-    quizChoices.innerHTML = "";
 
-    for (var i = 0; i < choices.choices1.length; i++) {
-        var listChoices = choices.choices1[i];
-    
-        var li = document.createElement("li");
+
+    document.getElementById("questions").innerHTML = exampleQues[0].question;
+    console.log(exampleQues);
+    //quizChoices.innerHTML = "";
+
+        for (var i = 0; i < exampleQues[0].choices.length; i++) {
+            var listChoices = exampleQues[0].choices[i];
         
-        li.setAttribute("data-index", i);
-    
-        var button = document.createElement("button");
-        button.textContent = listChoices;
-    
-        li.appendChild(button);
-        quizChoices.appendChild(li);
-      }
-    
+            var li = document.createElement("li");
+            
+            li.setAttribute("data-index", i);
+        
+            var button = document.createElement("button");
+            button.textContent = listChoices;
+        
+            li.appendChild(button);
+            quizChoices.appendChild(li);
+        }
+        quizChoices.addEventListener("click", function(event){
+            if(event.target.matches("button")){
+                        
+                var answer = event.target.matches("button");
+                console.log('event', event.target); 
+            }  
+            
+            console.log('answer', answer);
+            // var answer = event.target.matches("button");
+            // if(answer === exampleQues[0].accuracy){
+            //     alert("you win");
+            // }
+            
+        });
     
 
 
